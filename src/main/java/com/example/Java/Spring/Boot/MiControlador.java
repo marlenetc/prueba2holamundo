2d67ec03fba0a780a1299eaf0e2503fcfaf1bbb0
marlenetc <91817709+marlenetc@users.noreply.github.com>
2024-02-27 17:09:21 -0600

    Add files via upload

---
+++ src/main/java/com/example/Java/Spring/Boot/MiControlador.java
@@ -0,0 +1,12 @@
+package com.example.Java.Spring.Boot;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MiControlador {
+    @GetMapping("/")
+    public String getMessage(){
+
+        return "¡Hola mundo!, soy Marlene";
+    }
+}
